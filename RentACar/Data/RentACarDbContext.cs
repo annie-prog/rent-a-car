f2a4fd68a01967d259c7753c99a85a49dfb35f12
ani_konarcheva@abv.bg <ani_konarcheva@abv.bg>
2022-03-25 17:43:01 +0200

    Update RentACarDbContext.cs

--- RentACar/Data/RentACarDbContext.cs
+++ RentACar/Data/RentACarDbContext.cs
@@ -4,11 +4,19 @@ using Microsoft.EntityFrameworkCore;
 
 namespace Data
 {
-    public class RentACarDbContext
+    public class RentACarDbContext : DbContext
     {      
         public virtual DbSet<User> Users { get; set; }
         public virtual DbSet<Car> Cars { get; set; }
         public virtual DbSet<Rents> Rents { get; set; }
+
+        protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+        {
+            if (!optionsBuilder.IsConfigured)
+            {
+                optionsBuilder.UseSqlServer("Server=.;Database=RentACarDb;Integrated Security=true;");
+            }
+        }
 
     }
     
