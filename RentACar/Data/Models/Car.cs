6f246bff8bff91dcd2a9e2d9753395f449d16889
ani_konarcheva@abv.bg <ani_konarcheva@abv.bg>
2022-04-06 19:27:11 +0300

    Completed cars

--- RentACar/Data/Models/Car.cs
+++ RentACar/Data/Models/Car.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
@@ -10,16 +11,21 @@ namespace Data.Entities
     {
         public int Id { get; set; }
 
+        [Required]
         public string Brand { get; set; }
 
+        [Required]
         public string Model { get; set; }
 
+        [Required]
         public int Year { get; set; }
 
+        [Required]
         public int CountPassengerSeats { get; set; }
 
         public string Description { get; set; }
 
+        [Required]
         public decimal PriceForDay { get; set; }
     }
 }
--- RentACar/WebApp/Controllers/CarsController.cs
+++ RentACar/WebApp/Controllers/CarsController.cs
@@ -57,7 +57,7 @@ namespace WebApp.Controllers
         // For more details, see http://go.microsoft.com/fwlink/?LinkId=317598.
         [HttpPost]
         [ValidateAntiForgeryToken]
-        public async Task<IActionResult> Create([Bind("Id,Brand,Model,Year,CountPassengerSeats,Description,PriceForDay")] Car car)
+        public async Task<IActionResult> Create([Bind("Id, Brand, Model, Year, CountPassengerSeats, Description, PriceForDay")] Car car)
         {
             if (ModelState.IsValid)
             {
@@ -90,7 +90,7 @@ namespace WebApp.Controllers
         // For more details, see http://go.microsoft.com/fwlink/?LinkId=317598.
         [HttpPost]
         [ValidateAntiForgeryToken]
-        public async Task<IActionResult> Edit(int id, [Bind("Id,Brand,Model,Year,CountPassengerSeats,Description,PriceForDay")] Car car)
+        public async Task<IActionResult> Edit(int id, [Bind("Id, Brand, Model, Year, CountPassengerSeats, Description, PriceForDay")] Car car)
         {
             if (id != car.Id)
             {
